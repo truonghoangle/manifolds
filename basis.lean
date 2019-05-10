e6d2fa84ea312d7303778e0390ea3668db98d286
truonghoangle <40373735+truonghoangle@users.noreply.github.com>
2019-05-10 23:04:28 +0200

    Add files via upload

--- basis.lean
+++ basis.lean
@@ -1,9 +1,17 @@
+/-
+Copyright (c) 2019 Joe Cool. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Hoang Le Truong.
+
+-/
+
 import  topology.constructions  
 import equiv
 open topological_space 
 
 noncomputable theory
 universes u v 
+
 variables {α : Type u} {β : Type v} 
 
 
@@ -12,17 +20,29 @@ structure Top :=
   (struct : topological_space carrier)
 
 namespace Top
+
 instance : has_coe_to_sort Top := ⟨Type*, Top.carrier⟩
 attribute [instance] Top.struct
+
 def restrict (X : Top) (s : set X) : Top := ⟨subtype s, by apply_instance⟩
+
 end Top
 
 
 
-def is_continuous [topological_space α] [topological_space β] (f : α →. β) := continuous (f.as_subtype)
+def is_continuous [topological_space α] [topological_space β] (f : α →. β) := 
+continuous (f.as_subtype)
 
 structure phomeo (X Y : Top) extends X ≃. Y :=
 (continuous_to_fun  : is_continuous to_fun)
 (continuous_inv_fun : is_continuous inv_fun)
 
 infixr ` ≃ₜ. `:25 := phomeo
+
+
+
+
+
+
+
+
--- equiv.lean
+++ equiv.lean
@@ -1,3 +1,10 @@
+/-
+Copyright (c) 2019 Joe Cool. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Hoang Le Truong.
+
+-/
+
 import data.equiv.algebra  algebra.module data.pfun
 import         analysis.normed_space.basic
 
@@ -6,19 +13,45 @@ variables {α : Type u} {β : Type v} {γ : Type w}
 open equiv 
 
 namespace equiv
+
+
 section instances
+
 variables (e : α ≃ β)  
 
 protected def has_scalar [has_scalar γ β] : has_scalar γ α := ⟨λ (a:γ)  x, e.symm ( a • e x)⟩
-lemma smul_def [has_scalar γ β] (a: γ) (x : α) :  @has_scalar.smul _ _ (equiv.has_scalar e) a x = e.symm ( a • e x) := rfl
+
+lemma smul_def [has_scalar γ β] (a: γ) (x : α) :
+  @has_scalar.smul _ _ (equiv.has_scalar e) a x = e.symm ( a • e x) := rfl
 
 protected def mul_action [monoid γ][mul_action γ  β] : mul_action γ  α :=
 { one_smul := by simp [smul_def],
   mul_smul := by simp [smul_def, mul_action.mul_smul],
   ..equiv.has_scalar e  }
 
+
+--protected def has_dist [has_dist β]: has_dist α := 
+--⟨ λ x y, @has_dist.dist _ _ (e x) (e y)   ⟩ 
+
+
+
+
+
+
 end instances
+
 end equiv
+
+
+def fun_one_equiv : (fin 1 → α) ≃ α :=
+{ to_fun := λ x,  x 0,
+  inv_fun := λ x,  (λ i:fin 1, x),
+  left_inv := by {intro, simp[], ext1, apply congr_arg,
+  have h:=fin.cases rfl (λ i, i.elim0),apply h},
+  right_inv := by{ intro, simp[]}}
+
+
+
 
 namespace pfun
 
@@ -37,6 +70,8 @@ namespace compatible
   protected lemma symm (h : f ~. g) : g ~. f := λx, (h x).symm
  end compatible
 
+
+
 end pfun
 
 
@@ -51,15 +86,30 @@ structure pequiv (α : Type*) (β : Type*) :=
 infixr ` ≃. `:25 := pequiv
 
 
+
+
+
+
+
+
+
 namespace roption
 noncomputable theory
 
+
 variable [normed_field α]
+
 instance : has_add (roption α) := ⟨ λ x y, ⟨x.dom ∧ y.dom, λ h, x.get (h.1)+ y.get (h.2)  ⟩  ⟩
+
 instance   :  has_zero (roption (α ))  :=⟨some (0:α)⟩
+
 instance   :  has_one (roption (α ))  :=⟨some (1:α)⟩
+
 instance : has_mul (roption α) := ⟨ λ x y, ⟨x.dom ∧ y.dom, λ h, x.get (h.1) * y.get (h.2)  ⟩  ⟩
+
 instance  : has_scalar α (roption α) := ⟨λ a f, ⟨f.dom, λ h, a* (f.get h) ⟩ ⟩
+
+
 instance : has_neg (roption α) := ⟨ λ x, ⟨x.dom , λ h, -x.get h ⟩  ⟩
 
 end roption
